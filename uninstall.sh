3419ef8971fe0b6191f38d1553d48ee2ef13ec54
Fırat Özden <fir4tozden@gmail.com>
2026-09-07 23:35:49 +0300

    v1.0

--- uninstall.sh
+++ uninstall.sh
@@ -178,9 +178,9 @@ uninstall_package dnscrypt-proxy
 systemctl enable systemd-resolved "${log_redirects}"
 systemctl start systemd-resolved "${log_redirects}"
 
-chattr -i /etc/resolv.conf "${log_redirects}"
-
 tee /etc/systemd/resolved.conf &> /dev/null <<< ""
+
+chattr -i /etc/resolv.conf "${log_redirects}"
 
 [ -f /run/systemd/resolve/stub-resolv.conf ] && ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf &> "${log_redirects}"
 
